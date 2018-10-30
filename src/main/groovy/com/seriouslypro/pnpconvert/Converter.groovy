b61eec55e7a750ee0a531652b8c6063f0fbd8a17
Dominic Clifton <me@dominicclifton.name>
2018-10-30 13:47:21 +0100

    Introduce abstraction for CSV input file handling.  Extract DipTrace specific input-file handling into DipTrace specific classes.

--- src/main/groovy/com/seriouslypro/pnpconvert/Converter.groovy
+++ src/main/groovy/com/seriouslypro/pnpconvert/Converter.groovy
@@ -7,6 +7,140 @@ import java.awt.*
 import java.text.ParseException
 import java.text.SimpleDateFormat
 
+interface CSVHeaderParser {
+    void parse(String[] headerValues)
+    Map<Object, CSVHeader> getHeaderMappings()
+}
+
+class ComponentPlacement {
+    String refdes
+    String pattern
+    Coordinate coordinate
+    String side
+    BigDecimal rotation
+    String value
+    String name
+}
+
+class DipTraceHeaderParser implements CSVHeaderParser {
+    Map<DipTraceCSVHeaders, CSVHeader> headerMappings
+
+    private Map<DipTraceCSVHeaders, CSVHeader> createHeaderMappings(String[] headerValues) {
+        Map<DipTraceCSVHeaders, CSVHeader> headerMappings = [:]
+        headerValues.eachWithIndex { String headerValue, int index ->
+            try {
+                DipTraceCSVHeaders dipTraceCSVHeader = DipTraceCSVHeaders.fromString(headerValue)
+                CSVHeader csvHeader = new CSVHeader()
+                csvHeader.index = index
+                headerMappings[dipTraceCSVHeader] = csvHeader
+            } catch (IllegalArgumentException) {
+                // ignore unknown header
+            }
+        }
+        headerMappings
+    }
+
+    private void verifyRequiredHeadersPresent(Map<DipTraceCSVHeaders, CSVHeader> dipTraceCSVHeadersCSVHeaderMap, String[] headerValues) {
+        def requiredDipTraceCSVHeaders = [
+                DipTraceCSVHeaders.REFDES,
+                DipTraceCSVHeaders.PATTERN,
+                DipTraceCSVHeaders.X,
+                DipTraceCSVHeaders.Y,
+                DipTraceCSVHeaders.SIDE,
+                DipTraceCSVHeaders.ROTATE,
+                DipTraceCSVHeaders.VALUE,
+                DipTraceCSVHeaders.NAME
+        ]
+
+        boolean haveRequiredHeaders = dipTraceCSVHeadersCSVHeaderMap.keySet().containsAll(
+                requiredDipTraceCSVHeaders
+        )
+
+        if (!haveRequiredHeaders) {
+            String requiredHeaders = requiredDipTraceCSVHeaders.collect {
+                it.value
+            }.toArray().join(',')
+
+            throw new ParseException("Input CSV file does not contail all required headers, required: '$requiredHeaders', found: '$headerValues'", 0)
+        }
+    }
+
+    @Override
+    void parse(String[] headerValues) {
+        headerMappings = createHeaderMappings(headerValues)
+
+        verifyRequiredHeadersPresent(headerMappings, headerValues)
+    }
+}
+
+interface CSVLineParser {
+    ComponentPlacement parse(Map<Object, CSVHeader> headerMappings, String[] rowValues)
+}
+
+class DipTraceLineParser implements CSVLineParser {
+    @Override
+    ComponentPlacement parse(Map<Object, CSVHeader> headerMappings, String[] rowValues) {
+
+        BigDecimal x = rowValues[headerMappings[DipTraceCSVHeaders.X].index] as BigDecimal
+        BigDecimal y = rowValues[headerMappings[DipTraceCSVHeaders.Y].index] as BigDecimal
+        Coordinate coordinate = new Coordinate(x: x, y: y)
+
+        ComponentPlacement c = new ComponentPlacement(
+            refdes: rowValues[headerMappings[DipTraceCSVHeaders.REFDES].index],
+            coordinate: coordinate,
+            pattern: rowValues[headerMappings[DipTraceCSVHeaders.PATTERN].index],
+            side: rowValues[headerMappings[DipTraceCSVHeaders.SIDE].index],
+            rotation: rowValues[headerMappings[DipTraceCSVHeaders.ROTATE].index] as BigDecimal,
+            value: rowValues[headerMappings[DipTraceCSVHeaders.VALUE].index],
+            name: rowValues[headerMappings[DipTraceCSVHeaders.NAME].index]
+        )
+
+        return c
+    }
+}
+
+class CSVInput {
+    Reader reader
+    CSVHeaderParser headerParser
+    CSVLineParser lineParser
+
+    CSVReader inputCSVReader
+
+    CSVInput(Reader reader, CSVHeaderParser headerParser, CSVLineParser lineParser) {
+        this.headerParser = headerParser
+        this.lineParser = lineParser
+        this.reader = reader
+
+        inputCSVReader = new CSVReader(reader, ',' as char)
+
+    }
+
+    void close() {
+        inputCSVReader.close()
+    }
+
+    void parseHeader() {
+        String[] inputHeaderValues = inputCSVReader.readNext()
+        headerParser.parse(inputHeaderValues)
+    }
+
+    void parseLines(Closure c) {
+        String[] line
+
+        while ((line = inputCSVReader.readNext()) != null) {
+            ComponentPlacement componentPlacement = lineParser.parse(headerParser.headerMappings, line)
+            c(componentPlacement, line)
+        }
+    }
+}
+
+class DipTraceCSVInput extends CSVInput {
+
+    DipTraceCSVInput(Reader reader) {
+        super(reader, new DipTraceHeaderParser(), new DipTraceLineParser())
+    }
+}
+
 class Converter {
 
     String inputFileName
@@ -36,18 +170,13 @@ class Converter {
 
         String transformFileName = outputPrefix + "-transformed.csv"
 
-
         Reader inputFileReader = new FileReader(inputFileName)
-        CSVReader inputCSVReader = new CSVReader(inputFileReader, ',' as char)
+        CSVInput csvInput = new DipTraceCSVInput(inputFileReader)
 
         Writer transformFileWriter = new FileWriter(transformFileName, append)
         CSVWriter transformCSVWriter = new CSVWriter(transformFileWriter, ',' as char)
 
-        String[] inputHeaderValues = inputCSVReader.readNext()
-
-        LinkedHashMap<DipTraceCSVHeaders, CSVHeader> inputHeaderMappings = createHeaderMappings(inputHeaderValues)
-
-        verifyRequiredHeadersPresent(inputHeaderMappings, inputHeaderValues)
+        csvInput.parseHeader()
 
         String[] outputHeaderRow = [
             DipTraceCSVHeaders.REFDES.value,
@@ -62,51 +191,39 @@ class Converter {
         transformCSVWriter.writeNext(outputHeaderRow)
 
 
-        String[] line
-        while ((line = inputCSVReader.readNext()) != null) {
-            String refdes = line[inputHeaderMappings[DipTraceCSVHeaders.REFDES].index]
-            String pattern = line[inputHeaderMappings[DipTraceCSVHeaders.PATTERN].index]
-            BigDecimal x = line[inputHeaderMappings[DipTraceCSVHeaders.X].index] as BigDecimal
-            BigDecimal y = line[inputHeaderMappings[DipTraceCSVHeaders.Y].index] as BigDecimal
-            String side = line[inputHeaderMappings[DipTraceCSVHeaders.SIDE].index]
-            BigDecimal rotation = line[inputHeaderMappings[DipTraceCSVHeaders.ROTATE].index] as BigDecimal
-            String value = line[inputHeaderMappings[DipTraceCSVHeaders.VALUE].index]
-            String name = line[inputHeaderMappings[DipTraceCSVHeaders.NAME].index]
+        csvInput.parseLines { ComponentPlacement componentPlacement, String[] line ->
 
+            renderer.drawPart(Color.RED, componentPlacement.coordinate, componentPlacement.refdes)
 
-            Coordinate c = new Coordinate(x: x, y: y)
+            Coordinate rotatedCoordinate = boardRotation.applyRotation(componentPlacement.coordinate)
+            BigDecimal rotatedRotation = (componentPlacement.rotation + boardRotation.degrees).remainder(360)
 
-            renderer.drawPart(Color.RED, c, refdes)
-
-            Coordinate rotatedCoordinate = boardRotation.applyRotation(c)
-            BigDecimal rotatedRotation = (rotation + boardRotation.degrees).remainder(360)
-
-            renderer.drawPart(Color.BLUE, rotatedCoordinate, refdes)
+            renderer.drawPart(Color.BLUE, rotatedCoordinate, componentPlacement.refdes)
 
             Coordinate relocatedCoordinate = rotatedCoordinate - boardRotation.origin
 
-            renderer.drawPart(Color.PINK, relocatedCoordinate, refdes)
+            renderer.drawPart(Color.PINK, relocatedCoordinate, componentPlacement.refdes)
 
             Coordinate relocatedCoordinateWithOffset = relocatedCoordinate + offset
 
-            renderer.drawPart(Color.GREEN, relocatedCoordinateWithOffset, refdes)
+            renderer.drawPart(Color.GREEN, relocatedCoordinateWithOffset, componentPlacement.refdes)
 
             String[] outputRow = [
-                refdes,
-                pattern,
+                componentPlacement.refdes,
+                componentPlacement.pattern,
                 relocatedCoordinate.x,
                 relocatedCoordinate.y,
-                side,
+                componentPlacement.side,
                 rotatedRotation,
-                value,
-                name
+                componentPlacement.value,
+                componentPlacement.name
             ]
             transformCSVWriter.writeNext(outputRow)
             System.out.println(line.join(",").padRight(80) + " -> " + outputRow.join(","))
         }
 
+        csvInput.close()
 
-        inputCSVReader.close()
         transformCSVWriter.close()
 
         String svgFileName = outputPrefix + ".svg"
@@ -124,47 +241,6 @@ class Converter {
 */
     }
 
-
-
-    private Map<DipTraceCSVHeaders, CSVHeader> createHeaderMappings(String[] headerValues) {
-        Map<DipTraceCSVHeaders, CSVHeader> headerMappings = [:]
-        headerValues.eachWithIndex { String headerValue, int index ->
-            try {
-                DipTraceCSVHeaders dipTraceCSVHeader = DipTraceCSVHeaders.fromString(headerValue)
-                CSVHeader csvHeader = new CSVHeader()
-                csvHeader.index = index
-                headerMappings[dipTraceCSVHeader] = csvHeader
-            } catch (IllegalArgumentException) {
-                // ignore unknown header
-            }
-        }
-        headerMappings
-    }
-
-    void verifyRequiredHeadersPresent(Map<DipTraceCSVHeaders, CSVHeader> dipTraceCSVHeadersCSVHeaderMap, String[] headerValues) {
-        def requiredDipTraceCSVHeaders = [
-                DipTraceCSVHeaders.REFDES,
-                DipTraceCSVHeaders.PATTERN,
-                DipTraceCSVHeaders.X,
-                DipTraceCSVHeaders.Y,
-                DipTraceCSVHeaders.SIDE,
-                DipTraceCSVHeaders.ROTATE,
-                DipTraceCSVHeaders.VALUE,
-                DipTraceCSVHeaders.NAME
-        ]
-
-        boolean haveRequiredHeaders = dipTraceCSVHeadersCSVHeaderMap.keySet().containsAll(
-                requiredDipTraceCSVHeaders
-        )
-
-        if (!haveRequiredHeaders) {
-            String requiredHeaders = requiredDipTraceCSVHeaders.collect {
-                it.value
-            }.toArray().join(',')
-
-            throw new ParseException("Input CSV file does not contail all required headers, required: '$requiredHeaders', found: '$headerValues'", 0)
-        }
-    }
 
     void writeHeader(FileWriter fileWriter) {
 
