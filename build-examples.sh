d2f81e3ac4d0e966cabadfac50f7eeed065f7d68
Dominic Clifton <me@dominicclifton.name>
2018-11-01 17:43:08 +0100

    Update example files.
    Note: example feeders and csv files don't have any of the components used in the example1.dip/.csv files so the generated .dpv file has no feeders or components defined.  This needs an update when time permits.

---
+++ build-examples.sh
@@ -0,0 +1,6 @@
+#!/bin/env sh
+
+PNPCONVERT=./build/install/pnpconvert/bin/pnpconvert
+
+$PNPCONVERT -i examples/example1.csv -o examples/example1-90-with-rails -f examples/feeders.csv -co examples/components.csv -r 90 -rx 70 -ry 0 -ox 5 -oy 5 -c
+$PNPCONVERT -i examples/example1.csv -o examples/example1-270-with-rails -f examples/feeders.csv -co examples/components.csv -r 270 -rx 70 -ry 0 -ox 105 -oy 75 -c
