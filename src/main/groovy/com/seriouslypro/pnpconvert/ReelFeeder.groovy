a95be1ee660a0e70f15712c6cedd1fd1ac0e8e82
Dominic Clifton <me@dominicclifton.name>
2018-11-04 21:10:47 +0100

    Prevent some feeder settings being printed twice when using toString

--- src/main/groovy/com/seriouslypro/pnpconvert/ReelFeeder.groovy
+++ src/main/groovy/com/seriouslypro/pnpconvert/ReelFeeder.groovy
@@ -3,7 +3,7 @@ package com.seriouslypro.pnpconvert
 import groovy.transform.EqualsAndHashCode
 import groovy.transform.ToString
 
-@ToString(includeNames = true, includePackage = false, includeSuper = true)
+@ToString(includeNames = true, includePackage = false)
 @EqualsAndHashCode(callSuper = true)
 class ReelFeeder extends Feeder {
     int tapeWidth
--- src/main/groovy/com/seriouslypro/pnpconvert/TrayFeeder.groovy
+++ src/main/groovy/com/seriouslypro/pnpconvert/TrayFeeder.groovy
@@ -3,7 +3,7 @@ package com.seriouslypro.pnpconvert
 import groovy.transform.EqualsAndHashCode
 import groovy.transform.ToString
 
-@ToString(includeNames = true, includePackage = false, includeSuper = true)
+@ToString(includeNames = true, includePackage = false)
 @EqualsAndHashCode(callSuper = true)
 class TrayFeeder extends Feeder {
     Tray tray
