daf008b4efde46c7ed228c8b941c87891426aa79
Dominic Clifton <me@dominicclifton.name>
2019-07-26 22:41:50 +0200

    Update example build scripts.
     Also note that the google spreadsheet was also updated to add the separate mount and take delay columns to go with this commit.

--- build-examples.sh
+++ build-examples.sh
@@ -4,3 +4,5 @@ PNPCONVERT=./build/install/pnpconvert/bin/pnpconvert
 
 $PNPCONVERT -i examples/example1.csv -o examples/example1-90-with-rails -f examples/feeders.csv -co examples/components.csv -t examples/trays.csv -r 90 -rx 70 -ry 0 -ox 5 -oy 5 -c
 $PNPCONVERT -i examples/example1.csv -o examples/example1-270-with-rails -f examples/feeders.csv -co examples/components.csv -t examples/trays.csv -r 270 -rx 70 -ry 0 -ox 105 -oy 75 -c
+$PNPCONVERT @examples/example1-job1.pnpconvert
+
