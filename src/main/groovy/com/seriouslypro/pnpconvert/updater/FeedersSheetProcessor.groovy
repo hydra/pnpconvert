0407d445b819bec4607fe40e44a77dfd1af929a6
Dominic Clifton <me@dominicclifton.name>
2020-06-13 19:51:56 +0200

    Updates to the FeederSheetProcessor

--- src/main/groovy/com/seriouslypro/pnpconvert/updater/FeedersSheetProcessor.groovy
+++ src/main/groovy/com/seriouslypro/pnpconvert/updater/FeedersSheetProcessor.groovy
@@ -5,8 +5,13 @@ import com.google.api.services.sheets.v4.model.GridRange
 import com.google.api.services.sheets.v4.model.Sheet
 import com.google.api.services.sheets.v4.model.Spreadsheet
 import com.google.api.services.sheets.v4.model.ValueRange
+import com.seriouslypro.pnpconvert.Feeders
 
 class FeedersSheetProcessor {
+
+    private static final int HEADER_ROW_COUNT = 1
+    private static final int ROWS_PER_BATCH = 50
+
     SheetProcessorResult process(Sheets service, Spreadsheet spreadsheet, Sheet sheet, DPVTable feedersTable) {
         SheetProcessorResult sheetProcessorResult = new SheetProcessorResult()
 
@@ -16,8 +21,8 @@ class FeedersSheetProcessor {
         int columnCount = sheet.getProperties().getGridProperties().getColumnCount()
         int rowCount = sheet.getProperties().getGridProperties().getRowCount()
 
-        if (rowCount < 2) {
-            // need header row + data row to do something useful
+        if (rowCount <= HEADER_ROW_COUNT) {
+            // need header row + data rows to do something useful
             return sheetProcessorResult
         }
 
@@ -26,21 +31,72 @@ class FeedersSheetProcessor {
         gridRange.setEndColumnIndex(columnCount - 1)
 
         gridRange.setStartRowIndex(0)
-        gridRange.setEndRowIndex(0)
+        gridRange.setEndRowIndex(HEADER_ROW_COUNT - 1)
 
-        String range = sheetTitle + '!' + GridRangeConverter.toString(gridRange)
+        String headersRange = sheetTitle + '!' + GridRangeConverter.toString(gridRange)
+        ValueRange headersValuesRangeResponse = service.spreadsheets().values().get(speadsheetId, headersRange).execute()
+        List<List<Object>> headersValues = headersValuesRangeResponse.getValues()
+        dumpRows(headersValues)
 
-        ValueRange valuesRange = service.spreadsheets().values().get(speadsheetId, range).execute()
+        SheetToDPVHeaderMapping sheetToEntryHeaderMapping = new SheetToDPVHeaderMapping(feedersTable.headers, headersValues.last() as List<String>)
 
-        feedersTable.entries.each { List<String> entryValues ->
-            processEntry(entryValues, sheetProcessorResult)
+        int rowsRemaining = rowCount - HEADER_ROW_COUNT
+        int nextStartRowIndex = gridRange.getEndRowIndex() + 1
+
+        while (rowsRemaining > 0) {
+            gridRange.setStartRowIndex(nextStartRowIndex)
+            int rowsToRetrieve = ROWS_PER_BATCH;
+            if (rowsToRetrieve > rowsRemaining) {
+                rowsToRetrieve = rowsRemaining
+            }
+            gridRange.setEndRowIndex(nextStartRowIndex + rowsToRetrieve - 1) // -1 for inclusive
+
+            String dataRange = sheetTitle + '!' + GridRangeConverter.toString(gridRange)
+            ValueRange dataValuesRangeResponse = service.spreadsheets().values().get(speadsheetId, dataRange).execute()
+
+            List<List<Object>> dataRowsValues = dataValuesRangeResponse.getValues()
+            dumpRows(dataRowsValues)
+
+            dataRowsValues.each { feederRowValues ->
+                feedersTable.entries.each { List<String> dpvFeederEntryValues ->
+                    processEntry(sheetToEntryHeaderMapping, feederRowValues as List<String>, dpvFeederEntryValues, sheetProcessorResult)
+                }
+            }
+
+            int rowsRetrieved = dataRowsValues.size()
+            nextStartRowIndex = gridRange.getStartRowIndex() + rowsRetrieved
+
+            rowsRemaining -= rowsRetrieved
         }
         return sheetProcessorResult
     }
 
-    void processEntry(List<String> entryValues, SheetProcessorResult sheetProcessorResult) {
-        // TODO match up the components in the entryValues against a row in feeders sheet and update positions (and later, other stuff too)
-        sheetProcessorResult.totalFeederCount++
+    static void dumpRows(List<List<Object>> rowsValues) {
+        rowsValues.each { rowValues ->
+            println(rowValues)
+        }
     }
 
+    void processEntry(SheetToDPVHeaderMapping sheetToEntryHeaderMapping, List<String> sheetFeederRowValues, List<String> dpvFeederEntryValues, SheetProcessorResult sheetProcessorResult) {
+        sheetProcessorResult.totalFeederCount++
+
+        int dpvIdIndex = sheetToEntryHeaderMapping.dpvIndex(DPVStationTableColumn.ID)
+        int sheetIdIndex = sheetToEntryHeaderMapping.sheetIndex(Feeders.FeederCSVColumn.ID)
+
+        boolean idMatched = dpvFeederEntryValues[dpvIdIndex] == sheetFeederRowValues[sheetIdIndex]
+        if (!idMatched) {
+            return
+        }
+
+        int dpvNoteIndex = sheetToEntryHeaderMapping.dpvIndex(DPVStationTableColumn.NOTE)
+        int sheetComponentNameIndex = sheetToEntryHeaderMapping.sheetIndex(Feeders.FeederCSVColumn.COMPONENT_NAME)
+
+        boolean componentNameMatched = dpvFeederEntryValues[dpvNoteIndex].startsWith(sheetFeederRowValues[sheetComponentNameIndex])
+        if (!componentNameMatched) {
+            return
+        }
+
+        // TODO update the sheet with the new values
+        sheetProcessorResult.updatedFeederCount++
+    }
 }
--- src/test/groovy/com/seriouslypro/pnpconvert/updater/FeedersSheetProcessorSpec.groovy
+++ src/test/groovy/com/seriouslypro/pnpconvert/updater/FeedersSheetProcessorSpec.groovy
@@ -5,6 +5,9 @@ import com.google.api.services.sheets.v4.model.GridProperties
 import com.google.api.services.sheets.v4.model.Sheet
 import com.google.api.services.sheets.v4.model.SheetProperties
 import com.google.api.services.sheets.v4.model.Spreadsheet
+import com.google.api.services.sheets.v4.model.ValueRange
+import org.junit.internal.builders.IgnoredBuilder
+import spock.lang.Ignore
 import spock.lang.Specification
 
 import static com.seriouslypro.pnpconvert.Feeders.FeederCSVColumn.*
@@ -13,7 +16,7 @@ class FeedersSheetProcessorSpec extends Specification {
 
     private static final String TEST_SPREADSHEET_ID = 'SPREADSHEET_ID'
     private static final String TEST_SHEET_TITLE = 'SHEET_TITLE'
-    private static final String TEST_SHEET_ID = "TEST"
+    public static final String TEST_COMPONENT_NAME = 'COMPONENT_NAME'
 
     def "process empty table"() {
         given:
@@ -61,7 +64,7 @@ class FeedersSheetProcessorSpec extends Specification {
 
         and:
             final int headerRowCount = 1
-            final int valueRowCount = 3
+            final int valueRowCount = 1
 
             GridProperties gridProperties = new GridProperties(columnCount: 4, rowCount: headerRowCount + valueRowCount)
 
@@ -69,14 +72,23 @@ class FeedersSheetProcessorSpec extends Specification {
 
         and:
             DPVTable feedersTable = new DPVTable()
-            feedersTable.headers = [ID, COMPONENT_NAME, X_OFFSET, Y_OFFSET]
-            feedersTable.entries = [['1', 'TEST_COMPONENT_NAME', '0.12', '0.62']]
+            feedersTable.headers = ["ID", "Note", "DeltX", "DeltY"]
+            feedersTable.entries = [['1', TEST_COMPONENT_NAME, '0.12', '0.62']]
 
         and:
             Sheets.Spreadsheets mockSpreadsheets = Mock(Sheets.Spreadsheets)
             Sheets.Spreadsheets.Values.Get mockGet = Mock(Sheets.Spreadsheets.Values.Get)
-            Sheets.Spreadsheets.Values mockValues = Mock(Sheets.Spreadsheets.Values)
-            Spreadsheet spreadsheetWithValues = new Spreadsheet()
+            Sheets.Spreadsheets.Values mockValuesResponse = Mock(Sheets.Spreadsheets.Values)
+
+        and: "use headers in a different order to the values in the dpv file"
+            ValueRange headersValueRangeResponse = new ValueRange()
+            List<List<Object>> headerValues = [["X Offset", "Y Offset", "Component Name", "ID"]]
+            headersValueRangeResponse.setValues(headerValues)
+
+        and: "use data that corresponds to the a feeder, same component and feeder id, but with different x/y co-ordinates"
+            ValueRange dataValueRangeResponse = new ValueRange()
+            List<List<Object>> dataValues = [['0.23', '0.73', TEST_COMPONENT_NAME, '1']]
+            dataValueRangeResponse.setValues(dataValues)
 
         and:
             FeedersSheetProcessor processor = new FeedersSheetProcessor()
@@ -86,15 +98,30 @@ class FeedersSheetProcessorSpec extends Specification {
 
         then: "header row should be retrieved"
             1 * mockSheetsService.spreadsheets() >> mockSpreadsheets
-            1 * mockSpreadsheets.values() >> mockValues
-            1 * mockValues.get(TEST_SPREADSHEET_ID, "SHEET_TITLE!A1:D1") >> mockGet
-            1 * mockGet.execute() >> spreadsheetWithValues
+            1 * mockSpreadsheets.values() >> mockValuesResponse
+            1 * mockValuesResponse.get(TEST_SPREADSHEET_ID, "SHEET_TITLE!A1:D1") >> mockGet
+            1 * mockGet.execute() >> headersValueRangeResponse
+            0 * _
+
+        then: "data row should be retrieved"
+            1 * mockSheetsService.spreadsheets() >> mockSpreadsheets
+            1 * mockSpreadsheets.values() >> mockValuesResponse
+            1 * mockValuesResponse.get(TEST_SPREADSHEET_ID, "SHEET_TITLE!A2:D2") >> mockGet
+            1 * mockGet.execute() >> dataValueRangeResponse
             0 * _
 
 // TODO
 //        and: "sheet should contain updated values for X/Y offset"
 
         then:
-            result == new SheetProcessorResult(totalFeederCount: 1, updatedFeederCount: 0)
+            result == new SheetProcessorResult(totalFeederCount: 1, updatedFeederCount: 1)
+    }
+
+    @Ignore
+    def "avoid errors when rate is limited"() {
+        /*
+            Requests must be batched to avoid rate limit, exception thrown is:
+            "com.google.api.client.googleapis.json.GoogleJsonResponseException: 429 Too Many Requests"
+         */
     }
 }
