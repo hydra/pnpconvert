0668c57932b2bbb92bc0ca1d1aa220d2947ba512
Dominic Clifton <me@dominicclifton.name>
2021-07-16 13:06:40 +0200

    Use rotation values that are not the same in both the output and input and that require conversion.
    The test didn't check that the values for rotation actually changed.

--- src/test/groovy/com/seriouslypro/pnpconvert/diptrace/DipTraceCSVInputSpec.groovy
+++ src/test/groovy/com/seriouslypro/pnpconvert/diptrace/DipTraceCSVInputSpec.groovy
@@ -18,15 +18,15 @@ class DipTraceCSVInputSpec extends Specification {
 
             String content =
                 "RefDes,Pattern,X (mm),Y (mm),Side,Rotate,Value,Name\n" +
-                "Q2,SOT23,24,52.25,Top,0,,MMBT4401LT1\n" +
-                "Q3,SOT23,41,52.25,Top,180,,MMBT4401LT1\n"
+                "Q2,SOT23,24,52.25,Top,90,,MMBT4401LT1\n" +
+                "Q3,SOT23,41,52.25,Top,270,,MMBT4401LT1\n"
 
             Reader reader = new StringReader(content)
 
         and:
             ArrayList<ComponentPlacement> expectedPlacements = [
-                new ComponentPlacement(refdes: "Q2", name: "MMBT4401LT1", value: "", pattern:"SOT23", coordinate: new Coordinate(x: 24, y: 52.25), side: PCBSide.TOP, rotation: 0),
-                new ComponentPlacement(refdes: "Q3", name: "MMBT4401LT1", value: "", pattern:"SOT23", coordinate: new Coordinate(x: 41, y: 52.25), side: PCBSide.TOP, rotation: 180)
+                new ComponentPlacement(refdes: "Q2", name: "MMBT4401LT1", value: "", pattern:"SOT23", coordinate: new Coordinate(x: 24, y: 52.25), side: PCBSide.TOP, rotation: 270),
+                new ComponentPlacement(refdes: "Q3", name: "MMBT4401LT1", value: "", pattern:"SOT23", coordinate: new Coordinate(x: 41, y: 52.25), side: PCBSide.TOP, rotation: 90)
             ]
 
         and:
