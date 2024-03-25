52201b70cec11c273e1ecbb9c642fc9a035a53ab
Dominic Clifton <me@dominicclifton.name>
2024-03-25 12:58:31 +0100

    Extract row matcher code, add unit tests.
    * A bug was found were the full description in the DPV was matched to a partial description in the sheet, it's supposed to be the other way around as only the desciption in the note field in the DPV is length limited/truncated.  tests added for this bug.

---
+++ src/main/groovy/com/seriouslypro/pnpconvert/updater/RowMatcher.groovy
@@ -0,0 +1,97 @@
+package com.seriouslypro.pnpconvert.updater
+
+import com.seriouslypro.pnpconvert.Feeders
+import com.seriouslypro.pnpconvert.MatchOption
+
+class RowMatcher {
+
+    static boolean match(Set<MatchOption> matchOptions, SheetToDPVHeaderMapping sheetToEntryHeaderMapping, List<String> sheetFeederRowValues, List<String> dpvFeederEntryValues) {
+
+        if (matchOptions.empty) {
+            throw new IllegalArgumentException("At least one match option is required")
+        }
+
+        if (matchOptions.contains(MatchOption.FLAG_ENABLED)) {
+            int sheetFlagsIndex = sheetToEntryHeaderMapping.sheetIndex(Feeders.FeederCSVColumn.FLAGS)
+
+            boolean enabledFlagMatched = !sheetFeederRowValues[sheetFlagsIndex].contains('!')
+            if (!enabledFlagMatched) {
+                return false
+            }
+        }
+
+        if (matchOptions.contains(MatchOption.FEEDER_ID)) {
+            int dpvIdIndex = sheetToEntryHeaderMapping.dpvIndex(DPVStationTableColumn.ID)
+            int sheetColumnIndex = sheetToEntryHeaderMapping.sheetIndex(Feeders.FeederCSVColumn.ID)
+
+            boolean idMatched = dpvFeederEntryValues[dpvIdIndex] == sheetFeederRowValues[sheetColumnIndex]
+            if (!idMatched) {
+                return false
+            }
+        }
+
+        // use of the note field is fragile due to the dynamic content, and constrains of DPV note field, e.g. maximum length.
+        // part code and manufacturer may not be present, require description to be present in order to detect
+        int dpvNoteIndex = sheetToEntryHeaderMapping.dpvIndex(DPVStationTableColumn.NOTE)
+        List<String> noteParts = dpvFeederEntryValues[dpvNoteIndex] ? dpvFeederEntryValues[dpvNoteIndex].split(";") : []
+
+        if (matchOptions.contains(MatchOption.PART_CODE)) {
+            int sheetColumnIndex = sheetToEntryHeaderMapping.sheetIndex(Feeders.FeederCSVColumn.PART_CODE)
+
+            if (noteParts.size() < 2) {
+                return false
+            }
+            String partCode = noteParts[0]
+
+            boolean partCodeMatched = partCode == sheetFeederRowValues[sheetColumnIndex]
+            if (!partCodeMatched) {
+                return false
+            }
+        }
+
+        if (matchOptions.contains(MatchOption.MANUFACTURER)) {
+            int sheetColumnIndex = sheetToEntryHeaderMapping.sheetIndex(Feeders.FeederCSVColumn.MANUFACTURER)
+
+            if (noteParts.size() < 2) {
+                return false
+            }
+            String partCode = noteParts[1]
+
+            boolean partCodeMatched = partCode == sheetFeederRowValues[sheetColumnIndex]
+            if (!partCodeMatched) {
+                return false
+            }
+        }
+
+        // if there are 1,2,3 or 4 parts the description is available, but it's index will either be 1 or 3 depending
+        // on the presence of part code and manufacturer
+        // the description may become trimmed in the DPV file due to the field's maximum length, so partial matches
+        // may match more rows than intended!
+
+        if (matchOptions.contains(MatchOption.DESCRIPTION)) {
+            int sheetComponentNameIndex = sheetToEntryHeaderMapping.sheetIndex(Feeders.FeederCSVColumn.DESCRIPTION)
+
+            int descriptionIndex
+            switch (noteParts.size()) {
+                case 1:
+                case 2:
+                    descriptionIndex = 0
+                    break
+                case 3:
+                case 4:
+                    descriptionIndex = 2
+                    break
+                default:
+                    return false
+            }
+            String description = noteParts[descriptionIndex]
+
+            boolean descriptionMatched = sheetFeederRowValues[sheetComponentNameIndex].startsWith(description)
+            if (!descriptionMatched) {
+                return false
+            }
+        }
+
+        true
+    }
+}
---
+++ src/test/groovy/com/seriouslypro/pnpconvert/updater/RowMatcherSpec.groovy
@@ -0,0 +1,142 @@
+package com.seriouslypro.pnpconvert.updater
+
+import com.seriouslypro.pnpconvert.MatchOption
+import spock.lang.Specification
+import spock.lang.Unroll
+
+class RowMatcherSpec extends Specification {
+
+    def 'match with feeder id'() {
+        given:
+            Set<MatchOption> matchOptions = [MatchOption.FEEDER_ID]
+
+        and:
+            List<String> sheetHeaders = ['X Offset', 'Y Offset', 'Description', 'ID', 'Flags']
+            List<String> sheetFeederRowValues = ['0.23', '0.73', 'DESCRIPTION', '1', ]
+
+        and:
+            List<String> dpvTableHeaders = ['ID', 'Note', 'DeltX', 'DeltY']
+            List<String> dpvFeederEntryValues = ['1', 'DESCRIPTION;A NOTE', '0.12', '0.62']
+
+        and:
+            SheetToDPVHeaderMapping sheetToEntryHeaderMapping = new SheetToDPVHeaderMapping(dpvTableHeaders, sheetHeaders)
+
+        when:
+            boolean result = RowMatcher.match(matchOptions, sheetToEntryHeaderMapping, sheetFeederRowValues, dpvFeederEntryValues)
+
+        then:
+            result
+    }
+
+    @Unroll
+    def 'match with description only - #scenario'() {
+        given:
+            Set<MatchOption> matchOptions = [MatchOption.DESCRIPTION]
+
+        and:
+            List<String> sheetHeaders = ['X Offset', 'Y Offset', 'Description', 'ID', 'Flags']
+            List<String> sheetFeederRowValues = ['0.23', '0.73', sheetDescription, 'OLD_ID', ]
+
+        and:
+            List<String> dpvTableHeaders = ['ID', 'Note', 'DeltX', 'DeltY']
+            List<String> dpvFeederEntryValues = ['NEW_ID', dpvNote, '0.12', '0.62']
+
+        and:
+            SheetToDPVHeaderMapping sheetToEntryHeaderMapping = new SheetToDPVHeaderMapping(dpvTableHeaders, sheetHeaders)
+
+        when:
+            boolean result = RowMatcher.match(matchOptions, sheetToEntryHeaderMapping, sheetFeederRowValues, dpvFeederEntryValues)
+
+        then:
+            result == expectedResult
+
+        where:
+            dpvNote                             | sheetDescription | expectedResult | scenario
+            'DESCRIPTION'                       | 'DESCRIPTION'    | true           | 'exact'
+            'DESCRI'                            | 'DESCRIPTION'    | true           | 'partial'
+            'DESCRIPTION'                       | 'DESCRI'         | false          | 'reverse partial (only the dpv note gets truncated, not the description in sheet)'
+            'DESCRIPTION;FEEDER_NOTE'           | 'DESCRIPTION'    | true           | 'exact, with feeder note'
+            'DESCRI;FEEDER_NOTE'                | 'DESCRIPTION'    | true           | 'partial, with feeder note'
+            'DESCRI;FEEDER_NOTE'                | 'DESCRIPTION'    | true           | 'partial, with feeder note'
+            'PART;MFGR;DESCRIPTION;FEEDER_NOTE' | 'DESCRIPTION'    | true           | 'exact, with part code, manufacturer and feeder note'
+            'PART;MFGR;DESCRI;FEEDER_NOTE'      | 'DESCRIPTION'    | true           | 'partial, with part code, manufacturer and feeder note'
+    }
+
+    def 'match with id and enabled flag'() {
+        given:
+            Set<MatchOption> matchOptions = [MatchOption.DESCRIPTION]
+
+        and:
+            List<String> sheetHeaders = ['X Offset', 'Y Offset', 'Description', 'ID', 'Flags']
+            List<String> sheetFeederRowValues = ['0.23', '0.73', 'DESCRIPTION', 'OLD_ID', ]
+
+        and:
+            List<String> dpvTableHeaders = ['ID', 'Note', 'DeltX', 'DeltY']
+            List<String> dpvFeederEntryValues = ['NEW_ID', 'DESCRIPT', '0.12', '0.62']
+
+        and:
+            SheetToDPVHeaderMapping sheetToEntryHeaderMapping = new SheetToDPVHeaderMapping(dpvTableHeaders, sheetHeaders)
+
+        when:
+            boolean result = RowMatcher.match(matchOptions, sheetToEntryHeaderMapping, sheetFeederRowValues, dpvFeederEntryValues)
+
+        then:
+            result
+    }
+
+    def 'do not match with id and enabled flag when feeder is disabled'() {
+        given:
+            Set<MatchOption> matchOptions = [MatchOption.FEEDER_ID, MatchOption.FLAG_ENABLED]
+
+        and:
+            List<String> sheetHeaders = ['X Offset', 'Y Offset', 'Description', 'ID', 'Flags']
+            List<String> sheetFeederRowValues = ['0.23', '0.73', 'DESCRIPTION', 'OLD_ID', '!']
+
+        and:
+            List<String> dpvTableHeaders = ['ID', 'Note', 'DeltX', 'DeltY']
+            List<String> dpvFeederEntryValues = ['NEW_ID', 'DESCRIPT', '0.12', '0.62']
+
+        and:
+            SheetToDPVHeaderMapping sheetToEntryHeaderMapping = new SheetToDPVHeaderMapping(dpvTableHeaders, sheetHeaders)
+
+        when:
+            boolean result = RowMatcher.match(matchOptions, sheetToEntryHeaderMapping, sheetFeederRowValues, dpvFeederEntryValues)
+
+        then:
+            !result
+    }
+
+    @Unroll
+    def 'match with part code and manufacturer - #scenario'() {
+        given:
+            Set<MatchOption> matchOptions = [MatchOption.PART_CODE, MatchOption.MANUFACTURER]
+
+        and:
+            List<String> sheetHeaders = ['X Offset', 'Y Offset', 'Description', 'ID', 'Flags', 'Part Code', 'Manufacturer']
+            List<String> sheetFeederRowValues = ['0.23', '0.73', 'OLD_DESCRIPTION', 'OLD_ID', null, 'ABCD', 'WXYZ']
+
+        and:
+            List<String> dpvTableHeaders = ['ID', 'Note', 'DeltX', 'DeltY']
+            List<String> dpvFeederEntryValues = ['NEW_ID', note, '0.12', '0.62']
+
+        and:
+            SheetToDPVHeaderMapping sheetToEntryHeaderMapping = new SheetToDPVHeaderMapping(dpvTableHeaders, sheetHeaders)
+
+        when:
+            boolean result = RowMatcher.match(matchOptions, sheetToEntryHeaderMapping, sheetFeederRowValues, dpvFeederEntryValues)
+
+        then:
+            result == expectedResult
+
+        where:
+            note                               | expectedResult | scenario
+            'ABCD;WXYZ;NEW_DESCRIPTION;A NOTE' | true           | '4 fields'
+            'ABCD;WXYZ;NEW_DESCRIPTION'        | true           | '3 fields'
+            'ABCD;WXYZ'                        | true           | '2 fields'
+            'ABCD'                             | false          | 'missing manufacturer'
+            null                               | false          | 'no description'
+            ''                                 | false          | 'empty description'
+            'ABCD;QWER'                        | false          | 'only part code matches'
+            'ASDF;WXYZ'                        | false          | 'only manufacturer matches'
+    }
+}
